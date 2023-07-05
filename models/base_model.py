26fec543005033213a2270f513469195cc1df373
Santiago Caritat <6317@holbertonstudents.com>
2023-07-05 09:45:54 -0700

    kwargs issues resolved

--- models/base_model.py
+++ models/base_model.py
@@ -14,6 +14,7 @@ class BaseModel:
             for k, v in kwargs.items():
                 if k == "created_at" or k == "updated_at":
                     v = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+                if k != "__class__":
                     setattr(self, k, v)
         else:
             self.created_at = datetime.now()
