e6f6cbbff1b2fe9e818515967fae7f97fda8caff
Santiago Caritat <6317@holbertonstudents.com>
2023-07-05 09:03:41 -0700

    base model kwargs update

--- models/base_model.py
+++ models/base_model.py
@@ -9,9 +9,18 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """defines all common attributes/methods for other classes"""
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for k, v in kwargs.items():
+                if k == "created_at":
+                    self.created_at = v
+                    v = str_create_at
+                if k == "updated_at":
+                    self.updated_at = v
+                    v = str_update_at
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """print class name, id and dict"""
