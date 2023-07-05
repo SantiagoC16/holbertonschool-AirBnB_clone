b7d5da9298737b07210ac90af6d34bfffb8c8d09
Santiago Caritat <6317@holbertonstudents.com>
2023-07-05 06:14:54 -0700

    base model updated_at intance fixes

--- models/base_model.py
+++ models/base_model.py
@@ -6,12 +6,12 @@ from datetime import datetime
 
 
 class BaseModel:
-    def __init__(self, id=uuid.uuid4(), created_at=None, updated_at=None):
+    def __init__(self):
         """defines all common attributes/methods for other classes"""
 
-        self.id = str(id)
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """print class name, id and dict"""
