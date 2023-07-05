da484ce89c8c33942881322d18953391b00001be
Santiago Caritat <6317@holbertonstudents.com>
2023-07-05 05:48:58 -0700

    base model task 3 complete

--- models/base_model.py
+++ models/base_model.py
@@ -6,19 +6,28 @@ import datetime
 
 
 class BaseModel:
-    def __init__(self):
-        """def init"""
+    def __init__(self, id=uuid.uuid4(), created_at=None, updated_at=None):
+        """defines all common attributes/methods for other classes"""
 
-        self.id = str(uuid.uuid4())
+        self.id = str(id)
         self.created_at = datetime.now()
-        self.updated_at = 
+        self.updated_at = self.created_at
 
     def __str__(self):
         """str"""
-        return "[{}] (<{}>) <{}>".format(BaseModel.__name__, self.id, self.__dict___)
-    
+        return "[{}] (<{}>) <{}>".format(self.__class__.__name__,
+                                         self.id, self.__dict___)
+
     def save(self):
-        """save"""
+        """updates the public instance attribute updated_at with
+        the current datetime"""
+
+        self.updated = datetime.now()
 
     def to_dict(self):
         """dict"""
+
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = self.__class__.__name__
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict["updated_at"] = self.updated_at.isoformat()
