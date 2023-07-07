d3f3f6782f84ca2d5a6bd17086fa6b8dd14cc8f1
Santiago Caritat <6317@holbertonstudents.com>
2023-07-07 10:23:34 -0700

    console show complete

--- console.py
+++ console.py
@@ -61,10 +61,28 @@ class HBNBCommand(cmd.Cmd):
         else:
             key = args[0] + "." + args[1]
             dicti = models.storage.all()
-            print("{} {}".format(args[0], dicti[key]))
+            if key in dicti:
+                print(dicti[key])
+            else:
+                print("** no instance found **")
 
     def do_destroy(self, input):
         """Deletes an instance based on the class name and id"""
+
+        args = input.split()
+        if len(args) < 1:
+            print("** class name missing **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        elif args[0] not in self.classes:
+            print("** class doesn't exist **")
+        else:
+            key = args[0] + "." + args[1]
+            dicti = models.storage.all()
+            if key in dicti:
+                print(dicti[key])
+            else:
+                print("** no instance found **")
 
     def do_all(self, input):
         """Prints all string representation of all instances based
