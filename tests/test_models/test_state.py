5269fccbb275f489a0bf805f497cfe857484dddc
SantiagoC16 <6317@holbertonstudents.com>
2023-07-08 13:12:37 -0700

    tests state

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""unitest for state model"""
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+from models.city import City
+
+
+class TestUser(unittest.TestCase):
+    def setUp(self):
+        self.city = City()
+
+    def test_exist(self):
+        """test create exist"""
+        self.assertTrue(hasattr(self.city, "name"))
+
+    def test_empty(self):
+        """test create empty attibute"""
+        self.assertEqual(self.city.name, "")
+
+if __name__ == '__main__':
+    unittest.main()
