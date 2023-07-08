52b7a1b9fc5f1b391af0fb0d55d474e352ec3a8f
SantiagoC16 <6317@holbertonstudents.com>
2023-07-08 13:15:41 -0700

    tests review

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""unitest for review model"""
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+from models.review import Review
+
+class TestUser(unittest.TestCase):
+    def setUp(self):
+        self.review = Review()
+
+    def test_exist(self):
+        """test create exist"""
+        self.assertTrue(hasattr(self.review, "place_id"))
+        self.assertTrue(hasattr(self.review, "user_id"))
+        self.assertTrue(hasattr(self.review, "text"))
+
+    def test_empty(self):
+        """test create empty attibute"""
+        self.assertEqual(self.review.place_id, "")
+        self.assertEqual(self.review.user_id, "")
+        self.assertEqual(self.review.text, "")
+
+if __name__ == '__main__':
+    unittest.main()
