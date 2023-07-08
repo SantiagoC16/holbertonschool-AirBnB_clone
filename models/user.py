3dfc81c1102b057c54c8d48ce65f86c70c4c9bd1
SantiagoC16 <6317@holbertonstudents.com>
2023-07-08 12:23:50 -0700

    tests file storage v1'

--- models/user.py
+++ models/user.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""Define a User class"""
+"""unittest for class User"""
+import unittest
+from models.user import User
 from models.base_model import BaseModel
-
 
 class User(BaseModel):
     """class that inherits from BaseModel"""
