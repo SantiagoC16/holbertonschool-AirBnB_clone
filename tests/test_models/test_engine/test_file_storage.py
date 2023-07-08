0cbbdfb0120ad0df4efe1db57cf3f332bb6ed818
SantiagoC16 <6317@holbertonstudents.com>
2023-07-08 12:06:53 -0700

    teste file storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,55 +1,104 @@
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, mock_open
 from io import StringIO
+import json
 from models.base_model import BaseModel
 from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 from models.engine.file_storage import FileStorage
-import json
 import os
 
 
 class TestFileStorage(unittest.TestCase):
+
     def setUp(self):
+        self.file_path = "file.json"
         self.storage = FileStorage()
-        self.user = User()
-        self.base_model = BaseModel()
 
     def tearDown(self):
-        self.storage = None
-        self.user = None
-        self.base_model = None
         try:
-            os.remove(FileStorage._FileStorage__file_path)
+            os.remove(self.file_path)
         except FileNotFoundError:
             pass
 
     def test_all(self):
-        self.assertEqual(self.storage.all(), {})
+        # Ensure all() returns the __objects dictionary
+        objects = self.storage.all()
+        self.assertIsInstance(objects, dict)
+        self.assertEqual(objects, self.storage._FileStorage__objects)
 
     def test_new(self):
-        self.storage.new(self.user)
-        key = "User.{}".format(self.user.id)
-        self.assertEqual(self.storage.all()[key], self.user)
+        # Ensure new() adds the object to the __objects dictionary
+        obj = BaseModel()
+        self.storage.new(obj)
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.assertIn(key, self.storage._FileStorage__objects)
+        self.assertEqual(self.storage._FileStorage__objects[key], obj)
+
+    def test_attributes(self):
+        """ Test __objects, __file_path """
+        self.assertEqual(type(self.file_storage._FileStorage__objects), dict)
+        self.assertEqual(type(self.file_storage._FileStorage__file_path), str)
 
     def test_save(self):
-        self.storage.new(self.user)
-        self.storage.new(self.base_model)
-
-        with patch('sys.stdout', new=StringIO()) as fake_out:
+        # Mock the open() function and check if json.dump() is called
+        with patch('builtins.open', mock_open()) as mock_file:
             self.storage.save()
-            expected_output = "{}\n".format(json.dumps({
-                "User.{}".format(self.user.id): self.user.to_dict(),
-                "BaseModel.{}".format(self.base_model.id): self.base_model.to_dict()
-            }))
-            self.assertEqual(fake_out.getvalue(), expected_output)
+            mock_file.assert_called_once_with(self.file_path, "w")
+            mock_file().write.assert_called_once()
+
+            # Check if the correct JSON data is written
+            json_data = json.loads(mock_file().write.call_args[0][0])
+            objects = self.storage.all()
+            for key, obj in objects.items():
+                self.assertIn(key, json_data)
+                self.assertEqual(json_data[key], obj.to_dict())
 
     def test_reload(self):
-        self.storage.new(self.user)
-        self.storage.save()
-        self.storage.reload()
-        key = "User.{}".format(self.user.id)
-        self.assertEqual(type(self.storage.all()[key]), User)
-        self.assertEqual(self.storage.all()[key].id, self.user.id)
+        # Prepare mock JSON data
+        json_data = {
+            "BaseModel.1234": {
+                "id": "1234",
+                "name": "Test",
+                "created_at": "2022-01-01T00:00:00",
+                "updated_at": "2022-01-01T00:00:00"
+            },
+            "User.5678": {
+                "id": "5678",
+                "email": "test@example.com",
+                "password": "password",
+                "created_at": "2022-01-01T00:00:00",
+                "updated_at": "2022-01-01T00:00:00"
+            }
+        }
+
+        # Mock the open() function and simulate reading the mock JSON data
+        with patch('builtins.open', mock_open(read_data=json.dumps(json_data))):
+            self.storage.reload()
+
+            # Check if objects were correctly loaded
+            objects = self.storage.all()
+            self.assertEqual(len(objects), 2)
+
+            base_model_key = "BaseModel.1234"
+            self.assertIn(base_model_key, objects)
+            base_model = objects[base_model_key]
+            self.assertIsInstance(base_model, BaseModel)
+            self.assertEqual(base_model.id, "1234")
+            self.assertEqual(base_model.name, "Test")
+
+            user_key = "User.5678"
+            self.assertIn(user_key, objects)
+            user = objects[user_key]
+            self.assertIsInstance(user, User)
+            self.assertEqual(user.id, "5678")
+            self.assertEqual(user.email, "test@example.com")
+            self.assertEqual(user.password, "password")
+
 
 if __name__ == '__main__':
     unittest.main()
